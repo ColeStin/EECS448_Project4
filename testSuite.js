04b27912b3d069ab48a3e9771f7b6b698db339b6
ColeStin <60800520+ColeStin@users.noreply.github.com>
2021-11-15 19:36:26 -0600

    creation of the testSuite.js file
    this will be the center file for testing all the code and integrity of the server/client/SQL

---
+++ testSuite.js
@@ -0,0 +1,2 @@
+//this will be the center of the test suite for the code, there will most likely be a testSuite.php file so we can check server relations and SQL connections
+//maybe : have a test table to check information flow from the sql db
